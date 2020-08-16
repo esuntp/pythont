f6d4caa6a005f9607e12abc633d58cfc49104952
Ehsan <eh.taghipour@gmail.com>
2020-08-16 19:04:51 +1000

    netmiko from file cisco_conf_file

---
+++ netmikoFile.py
@@ -0,0 +1,16 @@
+from netmiko import ConnectHandler
+
+ios_l2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.21',
+    'username': 'ehsan',
+    'password': 'cisco',
+}
+
+with open('cisco_conf_file') as configFile:
+    lines = configFile.read()
+print(lines)
+
+net_connect = ConnectHandler(**ios_l2)
+output = net_connect.send_command(lines)
+print(output)
